7ec4149a3e31e6511e99b78a15e2e8474482f762
Samuel Lindgren <samuel.lindgren@softsense.io>
2024-11-25 16:27:58 +0100

    use cql syntax

--- schema/postgres/cadence/versioned/v0.6/executions_workflow_id.sql
+++ schema/postgres/cadence/versioned/v0.6/executions_workflow_id.sql
@@ -1,29 +1,19 @@
-ALTER TABLE executions
-ALTER COLUMN workflow_id TYPE text ;
+ALTER TABLE executions ALTER workflow_id TYPE text;
 
-ALTER TABLE current_executions
-ALTER COLUMN workflow_id TYPE  text;
+ALTER TABLE current_executions ALTER workflow_id TYPE text;
 
-ALTER TABLE buffered_events
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE buffered_events ALTER workflow_id TYPE text;
 
-ALTER TABLE activity_info_maps
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE activity_info_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE  timer_info_maps
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE timer_info_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE child_execution_info_maps
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE child_execution_info_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE request_cancel_info_maps
-ALTER COLUMN workflow_id TYPE  text;
+ALTER TABLE request_cancel_info_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE signal_info_maps
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE signal_info_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE buffered_replication_task_maps
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE buffered_replication_task_maps ALTER workflow_id TYPE text;
 
-ALTER TABLE signals_requested_sets
-ALTER COLUMN workflow_id TYPE text;
+ALTER TABLE signals_requested_sets ALTER workflow_id TYPE text;
