df076f6fd9e70d907d2740d15713f6610df84919
Neil Xie <neil.xie@uber.com>
2025-01-14 12:38:58 -0800

    Deleted extra code by accident

--- service/matching/handler/engine.go
+++ service/matching/handler/engine.go
@@ -539,6 +539,7 @@ pollLoop:
 		if err != nil {
 			return nil, fmt.Errorf("couldn't load tasklist namanger: %w", err)
 		}
+		startT := time.Now() // Record the start time
 		task, err := tlMgr.GetTask(pollerCtx, nil)
 		if err != nil {
 			// TODO: Is empty poll the best reply for errPumpClosed?
@@ -727,6 +728,7 @@ pollLoop:
 		if err != nil {
 			return nil, fmt.Errorf("couldn't load tasklist namanger: %w", err)
 		}
+		startT := time.Now() // Record the start time
 		task, err := tlMgr.GetTask(pollerCtx, maxDispatch)
 		if err != nil {
 			// TODO: Is empty poll the best reply for errPumpClosed?
