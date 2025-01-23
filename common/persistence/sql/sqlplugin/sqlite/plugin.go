9c224114c1e9cdd0fc5c4dc54caf2544818becea
Seva Kaloshin <vsevolod.kaloshin@gmail.com>
2025-01-23 13:58:20 +0100

    initialize plugin

--- common/persistence/sql/sqlplugin/sqlite/plugin.go
+++ common/persistence/sql/sqlplugin/sqlite/plugin.go
@@ -22,6 +22,35 @@
 
 package sqlite
 
+import (
+	"github.com/uber/cadence/common/config"
+	"github.com/uber/cadence/common/persistence/sql"
+	"github.com/uber/cadence/common/persistence/sql/sqlplugin"
+)
+
 const (
 	PluginName = "sqlite"
 )
+
+type plugin struct{}
+
+var _ sqlplugin.Plugin = (*plugin)(nil)
+
+func init() {
+	sql.RegisterPlugin(PluginName, &plugin{})
+}
+
+// CreateDB wraps createDB to return an instance of sqlplugin.DB
+func (p *plugin) CreateDB(cfg *config.SQL) (sqlplugin.DB, error) {
+	return p.createDB(cfg)
+}
+
+// CreateAdminDB wraps createDB to return an instance of sqlplugin.AdminDB
+func (p *plugin) CreateAdminDB(cfg *config.SQL) (sqlplugin.AdminDB, error) {
+	return p.createDB(cfg)
+}
+
+// createDB create a new instance of db
+func (p *plugin) createDB(cfg *config.SQL) (*db, error) {
+	return nil, nil
+}
