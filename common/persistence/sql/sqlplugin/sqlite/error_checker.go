b0da4cbf0a6ded8157ba64a5b6e6e29477919636
Seva Kaloshin <vsevolod.kaloshin@gmail.com>
2025-01-23 12:38:29 +0100

    fix lint issues

--- common/persistence/sql/sqlplugin/sqlite/error_checker.go
+++ common/persistence/sql/sqlplugin/sqlite/error_checker.go
@@ -24,24 +24,24 @@ package sqlite
 
 // IsDupEntryError verify if the error is a duplicate entry error
 func (mdb *db) IsDupEntryError(err error) bool {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 // IsNotFoundError verify if the error is a not found error
 func (mdb *db) IsNotFoundError(err error) bool {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 // IsTimeoutError verify if the error is a timeout error
 func (mdb *db) IsTimeoutError(err error) bool {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 // IsThrottlingError verify if the error is a throttling error
 func (mdb *db) IsThrottlingError(err error) bool {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
