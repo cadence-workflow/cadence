f442c444ba01421a86ccd6f132b1de8a0c6ccf7c
Seva Kaloshin <vsevolod.kaloshin@gmail.com>
2025-01-24 14:15:52 +0100

    make sqlite.DB inheriting mysql.DB

--- common/persistence/sql/sqlplugin/sqlite/plugin.go
+++ common/persistence/sql/sqlplugin/sqlite/plugin.go
@@ -24,7 +24,6 @@ package sqlite
 
 import (
 	"github.com/uber/cadence/common/config"
-	"github.com/uber/cadence/common/persistence/sql"
 	"github.com/uber/cadence/common/persistence/sql/sqlplugin"
 )
 
@@ -36,10 +35,6 @@ type plugin struct{}
 
 var _ sqlplugin.Plugin = (*plugin)(nil)
 
-func init() {
-	sql.RegisterPlugin(PluginName, &plugin{})
-}
-
 // CreateDB wraps createDB to return an instance of sqlplugin.DB
 func (p *plugin) CreateDB(cfg *config.SQL) (sqlplugin.DB, error) {
 	return p.createDB(cfg)
@@ -50,7 +45,7 @@ func (p *plugin) CreateAdminDB(cfg *config.SQL) (sqlplugin.AdminDB, error) {
 	return p.createDB(cfg)
 }
 
-// createDB create a new instance of db
-func (p *plugin) createDB(cfg *config.SQL) (*db, error) {
+// createDB create a new instance of DB
+func (p *plugin) createDB(cfg *config.SQL) (*DB, error) {
 	return nil, nil
 }
