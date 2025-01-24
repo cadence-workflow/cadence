01d91ad2310bbeb347373dd588e07d90e9535add
Seva Kaloshin <vsevolod.kaloshin@gmail.com>
2025-01-24 14:15:52 +0100

    make sqlite.DB inheriting mysql.DB

--- common/persistence/sql/sqlplugin/sqlite/db.go
+++ common/persistence/sql/sqlplugin/sqlite/db.go
@@ -23,90 +23,24 @@
 package sqlite
 
 import (
-	"context"
-	"time"
-
-	"github.com/jmoiron/sqlx"
-
-	"github.com/uber/cadence/common/persistence/sql/sqldriver"
 	"github.com/uber/cadence/common/persistence/sql/sqlplugin"
+	"github.com/uber/cadence/common/persistence/sql/sqlplugin/mysql"
 )
 
 var (
-	_ sqlplugin.AdminDB = (*db)(nil)
-	_ sqlplugin.DB      = (*db)(nil)
-	_ sqlplugin.Tx      = (*db)(nil)
+	_ sqlplugin.AdminDB = (*DB)(nil)
+	_ sqlplugin.DB      = (*DB)(nil)
+	_ sqlplugin.Tx      = (*DB)(nil)
 )
 
-type db struct {
-	converter   DataConverter
-	driver      sqldriver.Driver
-	originalDBs []*sqlx.DB
-	numDBShards int
-}
-
-// newDB returns a new instance of db
-func newDB(xdbs []*sqlx.DB, tx *sqlx.Tx, dbShardID int, numDBShards int) (*db, error) {
-	driver, err := sqldriver.NewDriver(xdbs, tx, dbShardID)
-	if err != nil {
-		return nil, err
-	}
-
-	db := &db{
-		converter:   &converter{},
-		driver:      driver,
-		originalDBs: xdbs, // this is kept because newDB will be called again when starting a transaction
-		numDBShards: numDBShards,
-	}
-
-	return db, nil
-}
-
-// GetTotalNumDBShards returns the total number of shards
-func (mdb *db) GetTotalNumDBShards() int {
-	return mdb.numDBShards
-}
-
-// BeginTx starts a new transaction and returns a reference to the Tx object
-func (mdb *db) BeginTx(ctx context.Context, dbShardID int) (sqlplugin.Tx, error) {
-	xtx, err := mdb.driver.BeginTxx(ctx, dbShardID, nil)
-	if err != nil {
-		return nil, err
-	}
-	return newDB(mdb.originalDBs, xtx, dbShardID, mdb.numDBShards)
+// DB contains methods for managing objects in a sqlite database
+// It inherits methods from the mysql.DB to reuse the implementation of the methods
+// sqlplugin.ErrorChecker is customized for sqlite
+type DB struct {
+	*mysql.DB
 }
 
 // PluginName returns the name of the plugin
-func (mdb *db) PluginName() string {
+func (mdb *DB) PluginName() string {
 	return PluginName
-}
-
-// Close closes the connection to the db
-func (mdb *db) Close() error {
-	return mdb.driver.Close()
-}
-
-// Commit commits a previously started transaction
-func (mdb *db) Commit() error {
-	return mdb.driver.Commit()
-}
-
-// Rollback triggers rollback of a previously started transaction
-func (mdb *db) Rollback() error {
-	return mdb.driver.Rollback()
-}
-
-// SupportsTTL returns weather SQLite supports TTL
-func (mdb *db) SupportsTTL() bool {
-	return false
-}
-
-// MaxAllowedTTL returns the max allowed ttl SQLite supports
-func (mdb *db) MaxAllowedTTL() (*time.Duration, error) {
-	return nil, sqlplugin.ErrTTLNotSupported
-}
-
-// SupportsAsyncTransaction returns weather SQLite supports Asynchronous transaction
-func (mdb *db) SupportsAsyncTransaction() bool {
-	return false
 }
--- common/persistence/sql/sqlplugin/sqlite/error_checker.go
+++ common/persistence/sql/sqlplugin/sqlite/error_checker.go
@@ -23,25 +23,25 @@
 package sqlite
 
 // IsDupEntryError verify if the error is a duplicate entry error
-func (mdb *db) IsDupEntryError(err error) bool {
+func (mdb *DB) IsDupEntryError(err error) bool {
 	// TODO implement me
 	panic("implement me")
 }
 
 // IsNotFoundError verify if the error is a not found error
-func (mdb *db) IsNotFoundError(err error) bool {
+func (mdb *DB) IsNotFoundError(err error) bool {
 	// TODO implement me
 	panic("implement me")
 }
 
 // IsTimeoutError verify if the error is a timeout error
-func (mdb *db) IsTimeoutError(err error) bool {
+func (mdb *DB) IsTimeoutError(err error) bool {
 	// TODO implement me
 	panic("implement me")
 }
 
 // IsThrottlingError verify if the error is a throttling error
-func (mdb *db) IsThrottlingError(err error) bool {
+func (mdb *DB) IsThrottlingError(err error) bool {
 	// TODO implement me
 	panic("implement me")
 }
