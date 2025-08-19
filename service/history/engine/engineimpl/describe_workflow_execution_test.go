430f2e0c7498b130d48906a66f9e0cadc0c98838
Chris Warren <16132615+c-warren@users.noreply.github.com>
2025-08-19 09:59:31 -0700

    Refactor the DescribeWorkflowExecution function

--- service/history/engine/engineimpl/describe_workflow_execution_test.go
+++ service/history/engine/engineimpl/describe_workflow_execution_test.go
@@ -31,9 +31,11 @@ import (
 	"github.com/stretchr/testify/mock"
 
 	"github.com/uber/cadence/common"
+	"github.com/uber/cadence/common/cache"
+	"github.com/uber/cadence/common/constants"
 	"github.com/uber/cadence/common/persistence"
 	"github.com/uber/cadence/common/types"
-	"github.com/uber/cadence/service/history/constants"
+	historyConstants "github.com/uber/cadence/service/history/constants"
 	"github.com/uber/cadence/service/history/engine/testdata"
 )
 
@@ -57,11 +59,11 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 			eft := testdata.NewEngineForTest(t, NewEngineWithShardContext)
 
 			childDomainID := "deleted-domain"
-			eft.ShardCtx.Resource.DomainCache.EXPECT().GetDomainName(constants.TestDomainID).Return(constants.TestDomainName, nil).AnyTimes()
+			eft.ShardCtx.Resource.DomainCache.EXPECT().GetDomainName(historyConstants.TestDomainID).Return(historyConstants.TestDomainName, nil).AnyTimes()
 			eft.ShardCtx.Resource.DomainCache.EXPECT().GetDomainName(childDomainID).Return("", &types.EntityNotExistsError{}).AnyTimes()
 			execution := types.WorkflowExecution{
-				WorkflowID: constants.TestWorkflowID,
-				RunID:      constants.TestRunID,
+				WorkflowID: historyConstants.TestWorkflowID,
+				RunID:      historyConstants.TestRunID,
 			}
 			parentWorkflowID := "parentWorkflowID"
 			parentRunID := "parentRunID"
@@ -149,7 +151,7 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 							},
 							StartedID:                2,
 							StartedTime:              time.Unix(0, *activity1.LastStartedTimestamp),
-							DomainID:                 constants.TestDomainID,
+							DomainID:                 historyConstants.TestDomainID,
 							ActivityID:               activity1.ActivityID,
 							Details:                  activity1.HeartbeatDetails,
 							LastHeartBeatUpdatedTime: time.Unix(0, *activity1.LastHeartbeatTimestamp),
@@ -175,11 +177,11 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 						},
 					},
 					ExecutionInfo: &persistence.WorkflowExecutionInfo{
-						DomainID:               constants.TestDomainID,
-						WorkflowID:             constants.TestWorkflowID,
-						RunID:                  constants.TestRunID,
+						DomainID:               historyConstants.TestDomainID,
+						WorkflowID:             historyConstants.TestWorkflowID,
+						RunID:                  historyConstants.TestRunID,
 						FirstExecutionRunID:    "first",
-						ParentDomainID:         constants.TestDomainID,
+						ParentDomainID:         historyConstants.TestDomainID,
 						ParentWorkflowID:       parentWorkflowID,
 						ParentRunID:            parentRunID,
 						InitiatedID:            initiatedID,
@@ -229,7 +231,7 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 
 			eft.Engine.Start()
 			result, err := eft.Engine.DescribeWorkflowExecution(ctx.Background(), &types.HistoryDescribeWorkflowExecutionRequest{
-				DomainUUID: constants.TestDomainID,
+				DomainUUID: historyConstants.TestDomainID,
 				Request: &types.DescribeWorkflowExecutionRequest{
 					Domain:    "why do we even have this field?",
 					Execution: &execution,
@@ -253,8 +255,8 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 					CloseTime:      closeTime,
 					CloseStatus:    types.WorkflowExecutionCloseStatusCompleted.Ptr(),
 					HistoryLength:  historyLength,
-					ParentDomainID: &constants.TestDomainID,
-					ParentDomain:   &constants.TestDomainName,
+					ParentDomainID: &historyConstants.TestDomainID,
+					ParentDomain:   &historyConstants.TestDomainName,
 					ParentExecution: &types.WorkflowExecution{
 						WorkflowID: parentWorkflowID,
 						RunID:      parentRunID,
@@ -271,7 +273,7 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 					IsCron:                       isCron,
 					UpdateTime:                   common.Int64Ptr(lastUpdatedTime.UnixNano()),
 					PartitionConfig:              partitionConfig,
-					CronOverlapPolicy:            &constants.CronSkip,
+					CronOverlapPolicy:            &historyConstants.CronSkip,
 					ActiveClusterSelectionPolicy: nil,
 				},
 				PendingActivities: []*types.PendingActivityInfo{
@@ -293,3 +295,346 @@ func TestDescribeWorkflowExecution(t *testing.T) {
 		})
 	}
 }
+
+func TestMapWorkflowExecutionConfiguration(t *testing.T) {
+	testCases := []struct {
+		name          string
+		executionInfo *persistence.WorkflowExecutionInfo
+		expected      *types.WorkflowExecutionConfiguration
+	}{
+		{
+			name: "Success - all fields present",
+			executionInfo: &persistence.WorkflowExecutionInfo{
+				TaskList:                    "test-task-list",
+				WorkflowTimeout:             1800,
+				DecisionStartToCloseTimeout: 30,
+			},
+			expected: &types.WorkflowExecutionConfiguration{
+				TaskList:                            &types.TaskList{Name: "test-task-list"},
+				ExecutionStartToCloseTimeoutSeconds: common.Int32Ptr(1800),
+				TaskStartToCloseTimeoutSeconds:      common.Int32Ptr(30),
+			},
+		},
+		{
+			name: "Success - zero values",
+			executionInfo: &persistence.WorkflowExecutionInfo{
+				TaskList:                    "",
+				WorkflowTimeout:             0,
+				DecisionStartToCloseTimeout: 0,
+			},
+			expected: &types.WorkflowExecutionConfiguration{
+				TaskList:                            &types.TaskList{Name: ""},
+				ExecutionStartToCloseTimeoutSeconds: common.Int32Ptr(0),
+				TaskStartToCloseTimeoutSeconds:      common.Int32Ptr(0),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := mapWorkflowExecutionConfiguration(tc.executionInfo)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+// TODO: More test cases with different states of the workflow
+func TestMapWorkflowExecutionInfo(t *testing.T) {
+	testCases := []struct {
+		name          string
+		executionInfo *persistence.WorkflowExecutionInfo
+		startEvent    *types.HistoryEvent
+		expectError   bool
+		expected      *types.WorkflowExecutionInfo
+	}{
+		{
+			name: "Success - basic workflow with parent",
+			executionInfo: &persistence.WorkflowExecutionInfo{
+				WorkflowID:                   "test-workflow-id",
+				RunID:                        "test-run-id",
+				TaskList:                     "test-task-list",
+				TaskListKind:                 types.TaskListKindNormal,
+				WorkflowTypeName:             "test-workflow-type",
+				StartTimestamp:               time.Unix(0, 1000000),
+				LastUpdatedTimestamp:         time.Unix(0, 2000000),
+				AutoResetPoints:              &types.ResetPoints{Points: []*types.ResetPointInfo{}},
+				Memo:                         map[string][]byte{"key": []byte("value")},
+				SearchAttributes:             map[string][]byte{"attr": []byte("val")},
+				PartitionConfig:              map[string]string{"partition": "config"},
+				CronOverlapPolicy:            historyConstants.CronSkip,
+				ActiveClusterSelectionPolicy: &types.ActiveClusterSelectionPolicy{},
+				ParentWorkflowID:             "parent-workflow-id",
+				ParentRunID:                  "parent-run-id",
+				ParentDomainID:               "parent-domain-id",
+				InitiatedID:                  123,
+				State:                        persistence.WorkflowStateCompleted,
+				CloseStatus:                  persistence.WorkflowCloseStatusCompleted,
+			},
+			startEvent: &types.HistoryEvent{
+				WorkflowExecutionStartedEventAttributes: &types.WorkflowExecutionStartedEventAttributes{
+					FirstDecisionTaskBackoffSeconds: common.Int32Ptr(10),
+				},
+			},
+			expectError: false,
+			expected: &types.WorkflowExecutionInfo{
+				Execution: &types.WorkflowExecution{
+					WorkflowID: "test-workflow-id",
+					RunID:      "test-run-id",
+				},
+				TaskList: &types.TaskList{
+					Name: "test-task-list",
+					Kind: types.TaskListKindNormal.Ptr(),
+				},
+				Type:                         &types.WorkflowType{Name: "test-workflow-type"},
+				StartTime:                    common.Int64Ptr(1000000),
+				HistoryLength:                10,
+				AutoResetPoints:              &types.ResetPoints{Points: []*types.ResetPointInfo{}},
+				Memo:                         &types.Memo{Fields: map[string][]byte{"key": []byte("value")}},
+				IsCron:                       false,
+				UpdateTime:                   common.Int64Ptr(2000000),
+				SearchAttributes:             &types.SearchAttributes{IndexedFields: map[string][]byte{"attr": []byte("val")}},
+				PartitionConfig:              map[string]string{"partition": "config"},
+				CronOverlapPolicy:            &historyConstants.CronSkip,
+				ActiveClusterSelectionPolicy: &types.ActiveClusterSelectionPolicy{},
+				ExecutionTime:                common.Int64Ptr(1000000 + (10 * time.Second).Nanoseconds()),
+				ParentExecution: &types.WorkflowExecution{
+					WorkflowID: "parent-workflow-id",
+					RunID:      "parent-run-id",
+				},
+				ParentDomainID:    common.StringPtr("parent-domain-id"),
+				ParentInitiatedID: common.Int64Ptr(123),
+				ParentDomain:      common.StringPtr("parent-domain-name"),
+				CloseStatus:       types.WorkflowExecutionCloseStatusCompleted.Ptr(),
+				CloseTime:         common.Int64Ptr(12345),
+			},
+		},
+		{
+			name: "Success - basic workflow without parent",
+			executionInfo: &persistence.WorkflowExecutionInfo{
+				WorkflowID:           "test-workflow-id",
+				RunID:                "test-run-id",
+				TaskList:             "test-task-list",
+				TaskListKind:         types.TaskListKindSticky,
+				WorkflowTypeName:     "test-workflow-type",
+				StartTimestamp:       time.Unix(0, 1000000),
+				LastUpdatedTimestamp: time.Unix(0, 2000000),
+				AutoResetPoints:      &types.ResetPoints{Points: []*types.ResetPointInfo{}},
+				Memo:                 map[string][]byte{},
+				SearchAttributes:     map[string][]byte{},
+				PartitionConfig:      map[string]string{},
+				CronSchedule:         "0 0 * * *",
+				State:                persistence.WorkflowStateRunning,
+			},
+			startEvent: &types.HistoryEvent{
+				WorkflowExecutionStartedEventAttributes: &types.WorkflowExecutionStartedEventAttributes{
+					FirstDecisionTaskBackoffSeconds: common.Int32Ptr(5),
+				},
+			},
+			expectError: false,
+			expected: &types.WorkflowExecutionInfo{
+				Execution: &types.WorkflowExecution{
+					WorkflowID: "test-workflow-id",
+					RunID:      "test-run-id",
+				},
+				TaskList: &types.TaskList{
+					Name: "test-task-list",
+					Kind: types.TaskListKindSticky.Ptr(),
+				},
+				Type:                         &types.WorkflowType{Name: "test-workflow-type"},
+				StartTime:                    common.Int64Ptr(1000000),
+				HistoryLength:                10,
+				AutoResetPoints:              &types.ResetPoints{Points: []*types.ResetPointInfo{}},
+				Memo:                         &types.Memo{Fields: map[string][]byte{}},
+				IsCron:                       true,
+				UpdateTime:                   common.Int64Ptr(2000000),
+				SearchAttributes:             &types.SearchAttributes{IndexedFields: map[string][]byte{}},
+				PartitionConfig:              map[string]string{},
+				CronOverlapPolicy:            &historyConstants.CronSkip,
+				ActiveClusterSelectionPolicy: nil,
+				ExecutionTime:                common.Int64Ptr(1000000 + (5 * time.Second).Nanoseconds()),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Create a simple mock that returns parent-domain-name for any domain ID
+			mockDomainCache := &testDomainCache{}
+
+			result, err := mapWorkflowExecutionInfo(tc.executionInfo, tc.startEvent, mockDomainCache, 10, &types.HistoryEvent{Timestamp: common.Int64Ptr(12345)})
+
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, result)
+			}
+		})
+	}
+}
+
+// TODO: Use a real domain cache mock if it exists elsewhere
+// Simple test mock for DomainCache
+type testDomainCache struct{}
+
+func (t *testDomainCache) GetDomainName(domainID string) (string, error) {
+	return "parent-domain-name", nil
+}
+
+func (t *testDomainCache) GetDomainID(domainName string) (string, error) {
+	return "test-domain-id", nil
+}
+
+func (t *testDomainCache) GetDomain(domainName string) (*cache.DomainCacheEntry, error) {
+	return nil, nil
+}
+
+func (t *testDomainCache) GetDomainByID(domainID string) (*cache.DomainCacheEntry, error) {
+	return nil, nil
+}
+
+func (t *testDomainCache) GetAllDomain() map[string]*cache.DomainCacheEntry {
+	return nil
+}
+
+func (t *testDomainCache) GetCacheSize() (sizeOfCacheByName int64, sizeOfCacheByID int64) {
+	return 0, 0
+}
+
+func (t *testDomainCache) RegisterDomainChangeCallback(id string, catchUpFn cache.CatchUpFn, prepareCallback cache.PrepareCallbackFn, callback cache.CallbackFn) {
+	// No-op for testing
+}
+
+func (t *testDomainCache) UnregisterDomainChangeCallback(id string) {
+	// No-op for testing
+}
+
+func (t *testDomainCache) Start() {
+	// No-op for testing
+}
+
+func (t *testDomainCache) Stop() {
+	// No-op for testing
+}
+
+// TODO: More test cases for any other branches
+func TestMapPendingActivityInfo(t *testing.T) {
+	testCases := []struct {
+		name                   string
+		activityInfo           *persistence.ActivityInfo
+		activityScheduledEvent *types.HistoryEvent
+		expected               *types.PendingActivityInfo
+	}{
+		{
+			name: "Success - started activity with retry policy",
+			activityInfo: &persistence.ActivityInfo{
+				ActivityID:               "test-activity-id",
+				ScheduleID:               123,
+				StartedID:                124,
+				CancelRequested:          false,
+				StartedTime:              time.Unix(0, 2001),
+				LastHeartBeatUpdatedTime: time.Unix(0, 2000),
+				Details:                  []byte("boom boom"),
+				HasRetryPolicy:           true,
+				Attempt:                  2002,
+				MaximumAttempts:          2003,
+				ExpirationTime:           time.Unix(0, 2004),
+				LastFailureReason:        "failure reason",
+				StartedIdentity:          "StartedWorkerIdentity",
+				LastWorkerIdentity:       "LastWorkerIdentity",
+				LastFailureDetails:       []byte("failure details"),
+				ScheduledTime:            time.Unix(0, 1999),
+			},
+			activityScheduledEvent: &types.HistoryEvent{
+				ActivityTaskScheduledEventAttributes: &types.ActivityTaskScheduledEventAttributes{
+					ActivityType: &types.ActivityType{
+						Name: "test-activity-type",
+					},
+				},
+			},
+			expected: &types.PendingActivityInfo{
+				ActivityID:             "test-activity-id",
+				ScheduleID:             123,
+				State:                  types.PendingActivityStateStarted.Ptr(),
+				HeartbeatDetails:       []byte("boom boom"),
+				LastHeartbeatTimestamp: common.Int64Ptr(2000),
+				LastStartedTimestamp:   common.Int64Ptr(2001),
+				Attempt:                2002,
+				MaximumAttempts:        2003,
+				ExpirationTimestamp:    common.Int64Ptr(2004),
+				LastFailureReason:      common.StringPtr("failure reason"),
+				StartedWorkerIdentity:  "StartedWorkerIdentity",
+				LastWorkerIdentity:     "LastWorkerIdentity",
+				LastFailureDetails:     []byte("failure details"),
+				ActivityType: &types.ActivityType{
+					Name: "test-activity-type",
+				},
+			},
+		},
+		{
+			name: "Success - scheduled activity without retry policy",
+			activityInfo: &persistence.ActivityInfo{
+				ActivityID:      "test-activity-id-2",
+				ScheduleID:      125,
+				StartedID:       constants.EmptyEventID,
+				CancelRequested: false,
+				ScheduledTime:   time.Unix(0, 1999),
+				HasRetryPolicy:  false,
+			},
+			activityScheduledEvent: &types.HistoryEvent{
+				ActivityTaskScheduledEventAttributes: &types.ActivityTaskScheduledEventAttributes{
+					ActivityType: &types.ActivityType{
+						Name: "test-activity-type-2",
+					},
+				},
+			},
+			expected: &types.PendingActivityInfo{
+				ActivityID:         "test-activity-id-2",
+				ScheduleID:         125,
+				State:              types.PendingActivityStateScheduled.Ptr(),
+				ScheduledTimestamp: common.Int64Ptr(1999),
+				ActivityType: &types.ActivityType{
+					Name: "test-activity-type-2",
+				},
+			},
+		},
+		{
+			name: "Success - cancel requested activity",
+			activityInfo: &persistence.ActivityInfo{
+				ActivityID:      "test-activity-id-3",
+				ScheduleID:      126,
+				StartedID:       127,
+				CancelRequested: true,
+				StartedTime:     time.Unix(0, 2001),
+				HasRetryPolicy:  false,
+			},
+			activityScheduledEvent: &types.HistoryEvent{
+				ActivityTaskScheduledEventAttributes: &types.ActivityTaskScheduledEventAttributes{
+					ActivityType: &types.ActivityType{
+						Name: "test-activity-type-3",
+					},
+				},
+			},
+			expected: &types.PendingActivityInfo{
+				ActivityID:           "test-activity-id-3",
+				ScheduleID:           126,
+				State:                types.PendingActivityStateCancelRequested.Ptr(),
+				LastStartedTimestamp: common.Int64Ptr(2001),
+				ActivityType: &types.ActivityType{
+					Name: "test-activity-type-3",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := mapPendingActivityInfo(tc.activityInfo, tc.activityScheduledEvent)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+// TODO: Test mapPendingChildExecutionInfo and mapPendingDecisionInfo
+// TODO: Test createDescribeWorkflowExecutionResponse - particularly the error cases
