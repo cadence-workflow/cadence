bd17ddae7b3634b6ae1f3786c15e766d792fe0d0
Bowen Xiao <xbowen@uber.com>
2025-09-15 17:16:16 -0700

    Add log when replication task latency is high
    Signed-off-by: Bowen Xiao <xbowen@uber.com>

--- service/history/replication/task_processor_test.go
+++ service/history/replication/task_processor_test.go
@@ -25,6 +25,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/uber/cadence/common/log"
 	"testing"
 	"time"
 
@@ -710,4 +711,28 @@ func TestIsShuttingDown(t *testing.T) {
 	assert.False(t, taskProcessor.isShuttingDown())
 	close(taskProcessor.done)
 	assert.True(t, taskProcessor.isShuttingDown())
+}
+
+func (s *taskProcessorSuite) TestProcessTaskOnce_OverThreshold_WithNoopLogger() {
+	s.taskExecutor.EXPECT().execute(gomock.Any(), false).Return(metrics.ScopeIdx(0), nil).Times(1)
+	// Domain cache is called for tagging
+	s.mockDomainCache.EXPECT().GetDomainName(testDomainID).Return(testDomainName, nil).AnyTimes()
+
+	// Use noop logger (no capturing, just to exercise the path)
+	s.taskProcessor.logger = log.NewNoop()
+
+	// Make e2e latency > threshold (e.g., 26 minutes)
+	creation := time.Now().Add(-26 * time.Minute).UnixNano()
+	task := &types.ReplicationTask{
+		TaskType: types.ReplicationTaskTypeHistoryV2.Ptr(),
+		HistoryTaskV2Attributes: &types.HistoryTaskV2Attributes{
+			DomainID:   testDomainID,
+			WorkflowID: testWorkflowID,
+			RunID:      testRunID,
+		},
+		CreationTime: common.Int64Ptr(creation),
+	}
+
+	err := s.taskProcessor.processTaskOnce(task)
+	s.NoError(err)
 }
