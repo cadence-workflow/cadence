554007a013fba8e3c48e4dbcb89d7df1db12a50b
David Porter <david.porter@uber.com>
2025-10-27 11:11:22 -0700

    WIP
    Signed-off-by: David Porter <david.porter@uber.com>

--- service/frontend/wrappers/grpc/api_generated.go
+++ service/frontend/wrappers/grpc/api_generated.go
@@ -61,11 +61,6 @@ func (g APIHandler) FailoverDomain(ctx context.Context, request *apiv1.FailoverD
 	return proto.FromFailoverDomainResponse(response), proto.FromError(err)
 }
 
-func (g APIHandler) ListFailoverHistory(ctx context.Context, request *apiv1.ListFailoverHistoryRequest) (*apiv1.ListFailoverHistoryResponse, error) {
-	response, err := g.h.ListFailoverHistory(ctx, proto.ToListFailoverHistoryRequest(request))
-	return proto.FromListFailoverHistoryResponse(response), proto.FromError(err)
-}
-
 func (g APIHandler) GetClusterInfo(ctx context.Context, request *apiv1.GetClusterInfoRequest) (*apiv1.GetClusterInfoResponse, error) {
 	response, err := g.h.GetClusterInfo(ctx)
 	return proto.FromGetClusterInfoResponse(response), proto.FromError(err)
@@ -99,6 +94,11 @@ func (g APIHandler) ListClosedWorkflowExecutions(ctx context.Context, request *a
 func (g APIHandler) ListDomains(ctx context.Context, request *apiv1.ListDomainsRequest) (*apiv1.ListDomainsResponse, error) {
 	response, err := g.h.ListDomains(ctx, proto.ToListDomainsRequest(request))
 	return proto.FromListDomainsResponse(response), proto.FromError(err)
+}
+
+func (g APIHandler) ListFailoverHistory(ctx context.Context, request *apiv1.ListFailoverHistoryRequest) (*apiv1.ListFailoverHistoryResponse, error) {
+	response, err := g.h.ListFailoverHistory(ctx, proto.ToListFailoverHistoryRequest(request))
+	return proto.FromListFailoverHistoryResponse(response), proto.FromError(err)
 }
 
 func (g APIHandler) ListOpenWorkflowExecutions(ctx context.Context, request *apiv1.ListOpenWorkflowExecutionsRequest) (*apiv1.ListOpenWorkflowExecutionsResponse, error) {
