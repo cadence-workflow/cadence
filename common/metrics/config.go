bf9b73872a86eb530c0ae59c3c0f255f7e9b674b
zawadzki <zawadzki@uber.com>
2026-02-18 21:55:06 +0000

    Add replication tasks lag migration keys
    Allow replication_tasks_lag timer and histogram names in migration config.
    
    Signed-off-by: zawadzki <zawadzki@uber.com>

--- common/metrics/config.go
+++ common/metrics/config.go
@@ -61,6 +61,8 @@ var HistogramMigrationMetrics = map[string]struct{}{
 	"replication_tasks_fetched_counts": {},
 	"replication_tasks_lag_raw":        {},
 	"replication_tasks_lag_raw_counts": {},
+	"replication_tasks_lag":            {},
+	"replication_tasks_lag_ns":         {},
 }
 
 func (h HistogramMigration) EmitTimer(name string) bool {
