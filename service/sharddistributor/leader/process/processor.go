22104629fd7eed5b646d2f33fef00162e06762c8
Andreas Holt <6665487+AndreasHolt@users.noreply.github.com>
2026-04-14 16:37:31 +0200

    refactor(sharddistributor): refactor naive load balance, to later allow for more clean switching between LB method and better directory structures
    Signed-off-by: Andreas Holt <6665487+AndreasHolt@users.noreply.github.com>

--- service/sharddistributor/leader/process/processor.go
+++ service/sharddistributor/leader/process/processor.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"maps"
-	"math"
 	"math/rand"
 	"slices"
 	"sort"
@@ -446,7 +445,14 @@ func (p *namespaceProcessor) rebalanceShardsImpl(ctx context.Context, metricsLoo
 	// If there are deleted shards or stale executors, the distribution has changed.
 	assignedToEmptyExecutors := assignShardsToEmptyExecutors(currentAssignments)
 	updatedAssignments := p.updateAssignments(shardsToReassign, activeExecutors, currentAssignments)
-	isRebalancedByShardLoad := p.rebalanceByShardLoad(calcShardLoad(namespaceState), currentAssignments, metricsLoopScope)
+	var isRebalancedByShardLoad bool
+	switch p.sdConfig.GetLoadBalancingMode(p.namespaceCfg.Name) {
+	case types.LoadBalancingModeNAIVE:
+		isRebalancedByShardLoad = p.rebalanceNaiveByReportedLoad(calcShardLoad(namespaceState), currentAssignments, metricsLoopScope)
+	default:
+		isRebalancedByShardLoad = p.rebalanceNaiveByReportedLoad(calcShardLoad(namespaceState), currentAssignments, metricsLoopScope)
+	}
+
 	p.emitExecutorMetric(namespaceState, metricsLoopScope)
 
 	distributionChanged := len(deletedShards) > 0 || len(staleExecutors) > 0 || assignedToEmptyExecutors || updatedAssignments || isRebalancedByShardLoad
@@ -587,99 +593,6 @@ func (*namespaceProcessor) updateAssignments(shardsToReassign []string, activeEx
 		currentAssignments[executorID] = append(currentAssignments[executorID], shardID)
 		i++
 	}
-
-	return true
-}
-
-// calcShardLoad returns a map of shardID to its load based on the latest reported shard loads from executors
-func calcShardLoad(namespaceState *store.NamespaceState) map[string]float64 {
-	shardLoad := make(map[string]float64)
-	for _, state := range namespaceState.Executors {
-		for shardID, report := range state.ReportedShards {
-			shardLoad[shardID] = report.ShardLoad
-		}
-	}
-	return shardLoad
-}
-
-// rebalanceByShardLoad does a rebalance if a difference between hottest and coldest executors' loads is more than maxDeviation
-// in this case the hottest shard will be moved to the coldest executor
-func (p *namespaceProcessor) rebalanceByShardLoad(shardLoad map[string]float64, currentAssignments map[string][]string, metricsScope metrics.Scope) (distributedChanged bool) {
-	// no rebalance if there are no more than 1 executor
-	if len(currentAssignments) < 2 {
-		return false
-	}
-
-	var (
-		hottestExecutorLoad = float64(0)
-		hottestExecutorID   = ""
-
-		hottestShardID   = ""
-		hottestShardLoad = float64(0)
-
-		coldestExecutorLoad = math.MaxFloat64
-		coldestExecutorID   = ""
-	)
-
-	// finding loads of hottest, coldest executors and hottest shard
-	executorLoad := make(map[string]float64)
-	for executorID, shardIDs := range currentAssignments {
-		for _, shardID := range shardIDs {
-			executorLoad[executorID] += shardLoad[shardID]
-		}
-
-		if executorLoad[executorID] <= coldestExecutorLoad {
-			coldestExecutorLoad = executorLoad[executorID]
-			coldestExecutorID = executorID
-		}
-
-		if executorLoad[executorID] >= hottestExecutorLoad {
-			hottestExecutorLoad = executorLoad[executorID]
-			hottestExecutorID = executorID
-
-			var maxShardLoad = float64(0)
-			for _, shardID := range shardIDs {
-				if shardLoad[shardID] >= maxShardLoad {
-					hottestShardID = shardID
-					maxShardLoad = shardLoad[shardID]
-				}
-			}
-			hottestShardLoad = maxShardLoad
-		}
-	}
-
-	// no rebalance if a deviation between coldest and hottest executors less than maxDeviation
-	if hottestExecutorLoad/coldestExecutorLoad < p.sdConfig.LoadBalancingNaive.MaxDeviation(p.namespaceCfg.Name) {
-		return false
-	}
-
-	// no rebalance if coldest executor becomes a hottest
-	if coldestExecutorLoad+hottestShardLoad >= hottestExecutorLoad {
-		return false
-	}
-
-	p.logger.Info("Load-based shard move",
-		tag.ShardKey(hottestShardID),
-		tag.ShardExecutor(hottestExecutorID),
-		tag.Dynamic("destination_executor", coldestExecutorID),
-		tag.ShardLoad(fmt.Sprintf("%f", hottestShardLoad)),
-		tag.Dynamic("hottest_executor_load", hottestExecutorLoad),
-		tag.Dynamic("coldest_executor_load", coldestExecutorLoad),
-		tag.Dynamic("load_ratio", hottestExecutorLoad/coldestExecutorLoad),
-		tag.Dynamic("hottest_executor_shard_count", len(currentAssignments[hottestExecutorID])),
-		tag.Dynamic("coldest_executor_shard_count", len(currentAssignments[coldestExecutorID])),
-	)
-	metricsScope.AddCounter(metrics.ShardDistributorAssignLoopLoadBasedMoves, 1)
-	metricsScope.UpdateGauge(metrics.ShardDistributorAssignLoopMovedShardLoad, hottestShardLoad)
-
-	// remove the hottest Shard from the hottest executor
-	// put it to the coldest executor
-	for i, shardID := range currentAssignments[hottestExecutorID] {
-		if shardID == hottestShardID {
-			currentAssignments[hottestExecutorID] = append(currentAssignments[hottestExecutorID][:i], currentAssignments[hottestExecutorID][i+1:]...)
-		}
-	}
-	currentAssignments[coldestExecutorID] = append(currentAssignments[coldestExecutorID], hottestShardID)
 
 	return true
 }
